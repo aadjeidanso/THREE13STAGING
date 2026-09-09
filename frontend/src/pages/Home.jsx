426853cc0bc1a0e5e8d364ec1a6553cbbe8fd0b9
aadjeidanso <adjeidansoemmanuel@gmail.com>
2026-09-08 20:02:49 -0400

    Improve registration dropdown menus

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -249,6 +249,63 @@ const referralSourceOptions = [
   'Other',
 ];
 
+const countryCodes = [
+  'AF', 'AX', 'AL', 'DZ', 'AS', 'AD', 'AO', 'AI', 'AQ', 'AG', 'AR', 'AM', 'AW', 'AU', 'AT', 'AZ',
+  'BS', 'BH', 'BD', 'BB', 'BY', 'BE', 'BZ', 'BJ', 'BM', 'BT', 'BO', 'BQ', 'BA', 'BW', 'BV', 'BR',
+  'IO', 'BN', 'BG', 'BF', 'BI', 'CV', 'KH', 'CM', 'CA', 'KY', 'CF', 'TD', 'CL', 'CN', 'CX', 'CC',
+  'CO', 'KM', 'CG', 'CD', 'CK', 'CR', 'CI', 'HR', 'CU', 'CW', 'CY', 'CZ', 'DK', 'DJ', 'DM', 'DO',
+  'EC', 'EG', 'SV', 'GQ', 'ER', 'EE', 'SZ', 'ET', 'FK', 'FO', 'FJ', 'FI', 'FR', 'GF', 'PF', 'TF',
+  'GA', 'GM', 'GE', 'DE', 'GH', 'GI', 'GR', 'GL', 'GD', 'GP', 'GU', 'GT', 'GG', 'GN', 'GW', 'GY',
+  'HT', 'HM', 'VA', 'HN', 'HK', 'HU', 'IS', 'IN', 'ID', 'IR', 'IQ', 'IE', 'IM', 'IL', 'IT', 'JM',
+  'JP', 'JE', 'JO', 'KZ', 'KE', 'KI', 'KP', 'KR', 'KW', 'KG', 'LA', 'LV', 'LB', 'LS', 'LR', 'LY',
+  'LI', 'LT', 'LU', 'MO', 'MG', 'MW', 'MY', 'MV', 'ML', 'MT', 'MH', 'MQ', 'MR', 'MU', 'YT', 'MX',
+  'FM', 'MD', 'MC', 'MN', 'ME', 'MS', 'MA', 'MZ', 'MM', 'NA', 'NR', 'NP', 'NL', 'NC', 'NZ', 'NI',
+  'NE', 'NG', 'NU', 'NF', 'MK', 'MP', 'NO', 'OM', 'PK', 'PW', 'PS', 'PA', 'PG', 'PY', 'PE', 'PH',
+  'PN', 'PL', 'PT', 'PR', 'QA', 'RE', 'RO', 'RU', 'RW', 'BL', 'SH', 'KN', 'LC', 'MF', 'PM', 'VC',
+  'WS', 'SM', 'ST', 'SA', 'SN', 'RS', 'SC', 'SL', 'SG', 'SX', 'SK', 'SI', 'SB', 'SO', 'ZA', 'GS',
+  'SS', 'ES', 'LK', 'SD', 'SR', 'SJ', 'SE', 'CH', 'SY', 'TW', 'TJ', 'TZ', 'TH', 'TL', 'TG', 'TK',
+  'TO', 'TT', 'TN', 'TR', 'TM', 'TC', 'TV', 'UG', 'UA', 'AE', 'GB', 'US', 'UM', 'UY', 'UZ', 'VU',
+  'VE', 'VN', 'VG', 'VI', 'WF', 'EH', 'YE', 'ZM', 'ZW',
+];
+
+const countryOptions = (() => {
+  try {
+    const displayNames = new Intl.DisplayNames(['en'], { type: 'region' });
+    return [...countryCodes.map((code) => displayNames.of(code)).filter(Boolean).sort((first, second) => first.localeCompare(second)), 'Other'];
+  } catch {
+    return ['United States', 'Canada', 'Ghana', 'Nigeria', 'United Kingdom', 'Other'];
+  }
+})();
+
+const registrationSelectMenuProps = {
+  anchorOrigin: {
+    vertical: 'bottom',
+    horizontal: 'left',
+  },
+  transformOrigin: {
+    vertical: 'top',
+    horizontal: 'left',
+  },
+  PaperProps: {
+    sx: {
+      maxHeight: 236,
+      overflowY: 'auto',
+      borderRadius: 1.5,
+      boxShadow: '0 18px 44px rgba(8,37,64,0.18)',
+      mt: 0.5,
+      '& .MuiMenuItem-root': {
+        minHeight: 38,
+        whiteSpace: 'normal',
+        alignItems: 'flex-start',
+      },
+    },
+  },
+  MenuListProps: {
+    dense: true,
+    sx: { py: 0.5 },
+  },
+};
+
 const chatAttachmentLimitMb = 5;
 const chatAttachmentLimitBytes = chatAttachmentLimitMb * 1024 * 1024;
 const apiBaseUrl = import.meta.env.VITE_API_BASE_URL || 'http://127.0.0.1:8000';
@@ -1392,17 +1449,22 @@ export default function Home() {
                     <TextField required fullWidth label="Last Name" name="lastName" value={formData.lastName} onChange={handleInputChange} error={Boolean(errors.lastName)} helperText={errors.lastName} size="small" />
                     <TextField required fullWidth label="Email Address" name="email" type="email" value={formData.email} onChange={handleInputChange} error={Boolean(errors.email)} helperText={errors.email} size="small" />
                     <TextField required fullWidth label="Phone Number" name="phone" value={formData.phone} onChange={handleInputChange} error={Boolean(errors.phone)} helperText={errors.phone} size="small" />
-                    <TextField fullWidth label="Country / Location" name="country" value={formData.country} onChange={handleInputChange} size="small" />
+                    <FormControl fullWidth size="small">
+                      <InputLabel>Country / Location</InputLabel>
+                      <Select name="country" value={formData.country} onChange={handleInputChange} label="Country / Location" MenuProps={registrationSelectMenuProps}>
+                        {countryOptions.map((option) => <MenuItem key={option} value={option}>{option}</MenuItem>)}
+                      </Select>
+                    </FormControl>
                     <FormControl fullWidth size="small" error={Boolean(errors.experienceLevel)}>
                       <InputLabel>Experience Level</InputLabel>
-                      <Select name="experienceLevel" value={formData.experienceLevel} onChange={handleInputChange} label="Experience Level">
+                      <Select name="experienceLevel" value={formData.experienceLevel} onChange={handleInputChange} label="Experience Level" MenuProps={registrationSelectMenuProps}>
                         {experienceLevelOptions.map((option) => <MenuItem key={option} value={option}>{option}</MenuItem>)}
                       </Select>
                       {errors.experienceLevel && <Typography color="error" sx={{ fontSize: 12, mt: 0.4 }}>{errors.experienceLevel}</Typography>}
                     </FormControl>
                     <FormControl fullWidth size="small" error={Boolean(errors.learningGoal)}>
                       <InputLabel>Learning Goal</InputLabel>
-                      <Select name="learningGoal" value={formData.learningGoal} onChange={handleInputChange} label="Learning Goal">
+                      <Select name="learningGoal" value={formData.learningGoal} onChange={handleInputChange} label="Learning Goal" MenuProps={registrationSelectMenuProps}>
                         {learningGoalOptions.map((option) => <MenuItem key={option} value={option}>{option}</MenuItem>)}
                       </Select>
                       {errors.learningGoal && <Typography color="error" sx={{ fontSize: 12, mt: 0.4 }}>{errors.learningGoal}</Typography>}
@@ -1412,7 +1474,7 @@ export default function Home() {
                     )}
                     <FormControl fullWidth size="small">
                       <InputLabel>How did you hear about Three13?</InputLabel>
-                      <Select name="referralSource" value={formData.referralSource} onChange={handleInputChange} label="How did you hear about Three13?">
+                      <Select name="referralSource" value={formData.referralSource} onChange={handleInputChange} label="How did you hear about Three13?" MenuProps={registrationSelectMenuProps}>
                         {referralSourceOptions.map((option) => <MenuItem key={option} value={option}>{option}</MenuItem>)}
                       </Select>
                     </FormControl>
